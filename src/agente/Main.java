0b483fafbbf218c678d6eba25bf797d39e784079
Marlon <Marlon@Marlon-NB>
2016-04-05 16:07:01 -0300

    Projeto 02 de Sistemas Inteligentes

---
+++ src/agente/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agente;
+
+/**
+ *
+ * @author Marlon Prudente <marlonoliveira@alunos.utfpr.edu.br>
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/ambiente/Grafo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ambiente;
+
+/**
+ *
+ * @author Marlon Prudente <marlonoliveira@alunos.utfpr.edu.br>
+ */
+public class Grafo {
+    public Grafo(){
+        Integer vertices;
+        Integer arestas;
+    }
+}
