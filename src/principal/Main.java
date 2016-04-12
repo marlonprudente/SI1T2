57c81ab7f520cb262b1e810ac9a8203176afec7a
Marlon <Marlon@Marlon-NB>
2016-04-11 22:34:43 -0300

    Update - Criando lista de vertices a partir do arquivo txt

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -4,26 +4,37 @@
  * and open the template in the editor.
  */
 package principal;
+
 import ambiente.Grafo;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Marlon Prudente <marlonoliveira@alunos.utfpr.edu.br>
  */
 public class Main {
+
     public static void main(String[] args) throws IOException {
         FileInputStream stream = new FileInputStream("grafo.txt");
         InputStreamReader streamReader = new InputStreamReader(stream);
         BufferedReader reader = new BufferedReader(streamReader);
         String linha;
-        
+        List<String> cidades = new ArrayList<String>();;
         while ((linha = reader.readLine()) != null) {
-            System.out.println(linha);
-            
-            
+            if(!linha.contains("---")) {
+                cidades.add(linha);
+            }
+            else{
+                System.out.println("PARE!");
+            }
         }
+        for(String s : cidades) {  
+            System.out.println(s);
+}
     }
 }
