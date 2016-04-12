2ca4d8ff7afca64048e11801c7028208a1e5602c
Marlon <Marlon@Marlon-NB>
2016-04-12 01:50:32 -0300

    Update - Crianto Matriz de adjacencia 50%

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -26,15 +27,40 @@ public class Main {
         String linha;
         List<String> cidades = new ArrayList<String>();;
         while ((linha = reader.readLine()) != null) {
-            if(!linha.contains("---")) {
+            if (!linha.contains(";")) {
                 cidades.add(linha);
-            }
-            else{
-                System.out.println("PARE!");
+            } 
+            else {
+                String[] t = linha.split(";");
+                System.out.println(t[0]);
+                System.out.println(t[1]);
+                System.out.println(t[2]);
+
             }
         }
-        for(String s : cidades) {  
+        //=====================================
+        int n = cidades.size()/2;
+        int[][] m = new int[n][n];
+        while ((linha = reader.readLine()) != null) {
+         if (linha.contains(";")) {
+                String[] t = linha.split(";");
+               m[Integer.parseInt(t[0])][Integer.parseInt(t[1])] = Integer.parseInt(t[2]);
+            } 
+
+        }
+        //======================================
+        System.out.println("Tam: " + cidades.size());
+        for (String s : cidades) {
+
             System.out.println(s);
-}
+
+        }
+        System.out.println("Tam: " + cidades.size());
+        for(int i = 0; i<n;i++){
+            System.out.println(" ");
+            for(int j = 0; j<n;j++){
+                System.out.print(m[i][j] + " ");
+            }
+        }
     }
 }
